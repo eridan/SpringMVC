ab2a0d224d5804992e3a241a5650ffb6f3fe4855
unknown <jurijspe@T61WorkLaptop.(none)>
2012-05-24 16:51:55 +0100

    Initial commit

---
+++ src/main/java/com/ibm/spring/mvc/controller/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ibm.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author jurijspe
+ */
+@Controller
+public class LoginController {
+    
+    @RequestMapping({"/","/index"})
+    public String showIndex() {
+        System.out.println("In the Controller. Index");
+        return "index";
+    }
+    
+    @RequestMapping("/login")
+    public String showLogin() {
+        System.out.println("In the Controller. Login");
+        return "login";
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : login
+    Created on : May 24, 2012, 4:44:24 PM
+    Author     : jurijspe
+--%>
+
+<%@page contentType="text/html" pageEncoding="windows-1252"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Login!</h1>
+    </body>
+</html>
