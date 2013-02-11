bb1ad21234a38ca0e423705b2a0f3f8245577835
eridan <jura_ir@mail.ru>
2013-02-11 15:16:45 +0000

    Update src/main/java/com/ibm/spring/mvc/controller/LoginController.java

--- src/main/java/com/ibm/spring/mvc/controller/LoginController.java
+++ src/main/java/com/ibm/spring/mvc/controller/LoginController.java
@@ -1,16 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ibm.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author jurijspe
- */
 @Controller
 public class LoginController {
     
